2bb84b81e10be35d972072952b1675811b3fad6d
amjadamju <amjadpanjeeri@gmail.com>
2020-05-01 14:52:56 +0530

    added book issued

---
+++ book_issued.java
@@ -0,0 +1,30 @@
+import java.awt.event.*;
+import java.awt.*;
+
+public class book_issued extends Frame {
+    Label heading;
+    Button ok;
+
+    book_issued() {
+        Label heading = new Label();
+        heading.setBounds(110, 60, 100, 30);
+        heading.setText("Book issued");
+        Button ok = new Button("OK");
+        ok.setBounds(130, 100, 50, 30);
+        add(heading);
+        add(ok);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+        setTitle("Message");
+        setSize(300, 200);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new book_issued();
+    }
+}
--- issue_book.java
+++ issue_book.java
@@ -1,10 +1,8 @@
 import java.awt.event.*;
-import java.util.Date;
 import java.awt.*;
 public class issue_book extends Frame{
     Label book_l,user_l,period_l,date_l;
-    TextField book_f,user_f,period_f;
-    Date date_f;
+    TextField book_f,user_f,period_f,date_f;
     Button submit;
     issue_book(){
 
@@ -20,7 +18,7 @@ public class issue_book extends Frame{
      period_l.setText("Periods(days)");
      period_l.setBounds(50, 140, 100, 30);
      date_l=new Label();
-     date_l.setText("Issued date(DD-MM-YYYY");
+     date_l.setText("Issued date(DD-MM-YYYY)");
      date_l.setBounds(50, 180, 160, 30);
 
      // Button
@@ -36,7 +34,8 @@ public class issue_book extends Frame{
      user_f.setBounds(160, 100, 200, 30);
      period_f=new TextField();
      period_f.setBounds(160, 140, 200, 30);
-     date_f=new Date();
+     date_f=new TextField();
+     date_f.setBounds(230,180,130,30);
      add(book_l);
      add(book_f);
      add(user_l);
@@ -44,8 +43,10 @@ public class issue_book extends Frame{
      add(period_l);
      add(period_f);
      add(date_l);
+     add(date_f);
      add(submit);
      setSize(400, 300);
+     setTitle("Enter Details");
      setLayout(null);
      setVisible(true);
      addWindowListener(new WindowAdapter(){
